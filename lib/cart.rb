1b5ce9ce7151a2fb1c7c1e6a7887f642e625a47e
callumahern <callum.ahern@simplybusiness.co.uk>
2022-08-02 11:25:30 +0100

    cart has items

--- lib/cart.rb
+++ lib/cart.rb
@@ -1,2 +1,8 @@
 class Cart
+
+  attr_reader :stock
+
+  def initialize(stock = {"apple": 5, "banana": 3, "coconut": 2, "dragonfruit": 1})
+    @stock = stock
+  end
 end
--- spec/cart_spec.rb
+++ spec/cart_spec.rb
@@ -4,4 +4,9 @@ RSpec.describe Cart do
   it 'is a cart' do
     expect(Cart.new).to be_a Cart
   end
+
+  it 'contains items' do
+    cart = Cart.new
+    expect(cart.stock[:apple]).to eq 5
+  end
 end
